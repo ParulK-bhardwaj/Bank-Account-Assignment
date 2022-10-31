691a119dc2bd4d655fa350067f33e90cb1babd1d
Parul Bhardwaj <bhardwaj.parul88@gmail.com>
2022-10-31 11:42:42 -0400

    New Account created

--- BankAccount.py
+++ BankAccount.py
@@ -8,6 +8,7 @@ class BankAccount:
 
     def deposit(self, amount):
         self.balance = self.balance + amount
+        self.balance_format = "%.2f" % float(self.balance)
         amount = "%.2f" % float(amount)
         print(f"Amount deposited: ${amount} New Balance: ${self.balance_format}.")
         
@@ -16,10 +17,12 @@ class BankAccount:
             print("Insufficient funds.")
             overdraft_fee = 10
             self.balance = self.balance - overdraft_fee
+            self.balance_format = "%.2f" % float(self.balance)
             overdraft_fee_format = "%.2f" % float(overdraft_fee)
             print(f"Overdraft fee: ${overdraft_fee_format} New Balance: ${self.balance_format} ")
         else:
             self.balance = self.balance - amount
+            self.balance_format = "%.2f" % float(self.balance)
             amount = "%.2f" % float(amount)
             print(f"Amount withdrawn: ${amount} New Balance: ${self.balance_format}.")
     
@@ -36,14 +39,27 @@ class BankAccount:
     # A string is a list in python so to call all the numbers from the last 4th element in the list. I called the -4 index to the last element in the string.
     def print_statement(self):
         truncated_account_number = "*" * (len(self.account_number)-4) + self.account_number[-4:]
+        self.balance_format = "%.2f" % float(self.balance)
         print(f"{self.full_name}\nAccount No.: {truncated_account_number}\nBalance: ${self.balance_format}")
 
 
 parul_account = BankAccount('parul Bhardwaj', '45678912', 4000)
-mitchell_account = BankAccount('Mitchell Hudson', '56712311', 10000)
+ivan_account = BankAccount('ivan dubey', '56712311', 10000)
 danielle_account = BankAccount('Danielle Roxberry', '12369349', 12000)
+mitchell_account = BankAccount('Mitchell hudson', '03141592', 0)
+
 # parul_account.get_balance()
 # parul_account.add_interest()
-parul_account.withdraw(4000.34)
-parul_account.deposit(300)
-parul_account.print_statement()
+# parul_account.withdraw(4000.34)
+# parul_account.withdraw(400)
+# parul_account.deposit(300)
+# parul_account.print_statement()
+print("\nMoney Deposited")
+mitchell_account.deposit(400000)
+mitchell_account.print_statement()
+print("\nInterest Added")
+mitchell_account.add_interest()
+mitchell_account.print_statement()
+print("\nMoney Withdrawn")
+mitchell_account.withdraw(150)
+mitchell_account.print_statement()
