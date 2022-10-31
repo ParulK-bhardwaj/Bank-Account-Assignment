8045ccf2b7f62513b2028c970072fcf4d9ff839f
Parul Bhardwaj <bhardwaj.parul88@gmail.com>
2022-10-31 16:00:50 -0400

    Updated account balance to be 0 at the start

--- BankAccount.py
+++ BankAccount.py
@@ -1,10 +1,9 @@
 class BankAccount:
-    def __init__(self, full_name, account_number, balance):
+    def __init__(self, full_name, account_number):
         self.full_name = full_name.title()
         self.account_number = account_number
-        self.balance = balance
-        self.balance_format = "%.2f" % float(self.balance)
-        
+        self.balance = 0
+        # self.account_type = account_type  
 
     def deposit(self, amount):
         self.balance = self.balance + amount
@@ -27,6 +26,7 @@ class BankAccount:
             print(f"Amount withdrawn: ${amount} New Balance: ${self.balance_format}.")
     
     def get_balance(self):
+        self.balance_format = "%.2f" % float(self.balance)
         print(f"Your Account Balance is ${self.balance_format}.")
         return self.balance
 
@@ -34,7 +34,6 @@ class BankAccount:
         interest = float(self.balance) * 0.00083
         self.balance = float(self.balance)+ interest
     
-    # -------------TODO float 2 decimal places---------------- 
     # Updated the name to titlel case using .title()
     # A string is a list in python so to call all the numbers from the last 4th element in the list. I called the -4 index to the last element in the string.
     def print_statement(self):
@@ -43,17 +42,17 @@ class BankAccount:
         print(f"{self.full_name}\nAccount No.: {truncated_account_number}\nBalance: ${self.balance_format}")
 
 
-parul_account = BankAccount('parul Bhardwaj', '45678912', 4000)
-ivan_account = BankAccount('ivan dubey', '56712311', 10000)
-danielle_account = BankAccount('Danielle Roxberry', '12369349', 12000)
-mitchell_account = BankAccount('Mitchell hudson', '03141592', 0)
+parul_account = BankAccount('parul Bhardwaj', '45678912')
+ivan_account = BankAccount('ivan dubey', '56712311')
+danielle_account = BankAccount('Danielle Roxberry', '12369349')
+mitchell_account = BankAccount('Mitchell hudson', '03141592')
 
-# parul_account.get_balance()
-# parul_account.add_interest()
-# parul_account.withdraw(4000.34)
-# parul_account.withdraw(400)
-# parul_account.deposit(300)
-# parul_account.print_statement()
+parul_account.get_balance()
+parul_account.add_interest()
+parul_account.withdraw(4000.34)
+parul_account.withdraw(400)
+parul_account.deposit(300)
+parul_account.print_statement()
 print("\nMoney Deposited")
 mitchell_account.deposit(400000)
 mitchell_account.print_statement()
