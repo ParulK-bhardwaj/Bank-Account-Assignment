2ae7f9372bc8555dbc1ab9bb730d16e5889478dd
Parul Bhardwaj <bhardwaj.parul88@gmail.com>
2022-10-30 21:16:03 -0400

    Account Number and Name update

--- BankAccount.py
+++ BankAccount.py
@@ -1,43 +1,49 @@
-from pytest import console_main
-
-
 class BankAccount:
     def __init__(self, full_name, account_number, balance):
-        self.full_name = full_name
+        self.full_name = full_name.title()
         self.account_number = account_number
         self.balance = balance
+        self.balance_format = "%.2f" % float(self.balance)
+        
 
     def deposit(self, amount):
         self.balance = self.balance + amount
-        print(f"Amount deposited: ${float(amount)} New Balance: ${float(self.balance)}.")
+        amount = "%.2f" % float(amount)
+        print(f"Amount deposited: ${amount} New Balance: ${self.balance_format}.")
         
     def withdraw(self, amount):
         if amount > self.balance:
             print("Insufficient funds.")
             overdraft_fee = 10
             self.balance = self.balance - overdraft_fee
-            print(f"Overdraft fee: ${overdraft_fee} New Balance: ${float(self.balance)} ")
+            overdraft_fee_format = "%.2f" % float(overdraft_fee)
+            print(f"Overdraft fee: ${overdraft_fee_format} New Balance: ${self.balance_format} ")
         else:
             self.balance = self.balance - amount
-            print(f"Amount withdrawn: ${float(amount)} New Balance: ${float(self.balance)}.")
+            amount = "%.2f" % float(amount)
+            print(f"Amount withdrawn: ${amount} New Balance: ${self.balance_format}.")
     
     def get_balance(self):
-        print(f"Your Account Balance is {self.balance}.")
+        print(f"Your Account Balance is ${self.balance_format}.")
         return self.balance
 
     def add_interest(self):
-        interest = self.balance * 0.00083
-        self.balance = self.balance + interest
+        interest = float(self.balance) * 0.00083
+        self.balance = float(self.balance)+ interest
     
     # -------------TODO float 2 decimal places---------------- 
-    # -------------TODO update the name to pascal case---------------- 
+    # Updated the name to titlel case using .title()
     # A string is a list in python so to call all the numbers from the last 4th element in the list. I called the -4 index to the last element in the string.
     def print_statement(self):
         truncated_account_number = "*" * (len(self.account_number)-4) + self.account_number[-4:]
-        print(f"{self.full_name}\nAccount No.: {truncated_account_number}\nBalance: ${float(self.balance)}")
+        print(f"{self.full_name}\nAccount No.: {truncated_account_number}\nBalance: ${self.balance_format}")
 
 
-parul_account = BankAccount('Parul Bhardwaj', '45678912', 4000)
+parul_account = BankAccount('parul Bhardwaj', '45678912', 4000)
 mitchell_account = BankAccount('Mitchell Hudson', '56712311', 10000)
 danielle_account = BankAccount('Danielle Roxberry', '12369349', 12000)
+# parul_account.get_balance()
+# parul_account.add_interest()
+parul_account.withdraw(4000.34)
+parul_account.deposit(300)
 parul_account.print_statement()
